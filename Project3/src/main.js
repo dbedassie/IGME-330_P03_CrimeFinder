b119a5739589e1b95e5eb383b68cf057d4109d15
Durrell Bedassie <durrellbedassie@gmail.com>
2020-11-22 23:44:58 -0500

    sidebar sort of working now
    just have to remove open button and make close button look better. style the sidebar to look prettier

--- Project3/src/main.js
+++ Project3/src/main.js
@@ -1,6 +1,20 @@
 import * as map from './map.js';
 
 function setupUI() {
+    let openBtn = document.querySelector("#openBtn");
+    let closeBtn = document.querySelector("#closeBtn");
+    let sidebar = document.querySelector("#sideBar");
+    let main = document.querySelector("#main");
+
+    openBtn.onclick = (e) => {
+        sidebar.style.width = "250px";
+        main.querySelector("#main").style.marginLeft = "250px";
+    }
+
+    closeBtn.onclick = (e) => {
+        sidebar.style.width = "0";
+        main.style.marginLeft = "0";
+    }
 }
 
 function init() {
