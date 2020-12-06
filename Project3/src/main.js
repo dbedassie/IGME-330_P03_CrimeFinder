19db7428e38fee24a8c1c4e030f0ceb320741cfd
Durrell Bedassie <durrellbedassie@gmail.com>
2020-12-06 00:17:44 -0500

    Added filter features
    need to hook up with API and code

--- Project3/src/main.js
+++ Project3/src/main.js
@@ -5,22 +5,40 @@ function setupUI() {
     let openBtn = document.querySelector("#openBtn");
     let sidebar = document.querySelector("#sideBar");
     let main = document.querySelector("#main");
+    let legend = document.querySelector("#legend");
     let open = false;
 
     openBtn.onclick = (e) => {
-        if(open) {
+        if (open) {
             sidebar.style.width = "250px";
             main.style.marginLeft = "250px";
-            openBtn.innerHTML = "&#9776; Close Sidebar"
             open = !sidebar;
+        } else if (!open) {
+            sidebar.style.width = "0";
+            main.style.marginLeft = "0";
+            open = !open;
         }
-        else if(!open) {
-                sidebar.style.width = "0";
-                main.style.marginLeft = "0";
-                openBtn.innerHTML = "&#9776; Open Sidebar";
-                open = !open;
-            }
-        
+
+    }
+
+    // Setting up the map legend
+    let starNum = ['A', 'B', 'C', 'D', 'E'];
+    let colors = ['#00FF00', '#7FFF00', '#FFFF00', '#FFF000', '#FF0000'];
+    
+    for (let i = 0; i < starNum.length; i++) {
+        let layer = starNum[i];
+        let color = colors[i];
+        let item = document.createElement('div');
+        let key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+
+        let value = document.createElement('span');
+        value.innerHTML = layer;
+        value.style.color = 'black';
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
     }
 }
 
@@ -28,8 +46,17 @@ function init() {
     map.initMap();
     map.loadMarkers();
     map.addMarkersToMap();
-   routes.getRoutesForLatLon(40.03,-105.25, 60,150,"5.6","5.10");
+    //routes.getRoutesForLatLon(40.03,-105.25, 60,150,"5.6","5.10");
     setupUI();
+
+    document.querySelector('#map').onclick = (e) => {
+        searchOnClick();
+    }
+
+}
+
+function searchOnClick() {
+    map.getMousePointer();
 }
 
 export {
--- Project3/src/map.js
+++ Project3/src/map.js
@@ -9,15 +9,17 @@ function initMap() {
 
     map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/light-v10',
-        center: [-96.50390625,
-          40.01078714046552],
+        style: 'mapbox://styles/mapbox/outdoors-v11',
         zoom: 4
     });
 
     // The 'building' layer in the mapbox-streets vector source contains building-height
     // data from OpenStreetMap.
     map.on('load', function () {
+        
+        // Make map center on the US
+        map.fitBounds([[-133.2421875, 16.972741], [-47.63671875, 52.696361]]);
+        
         // Insert the layer beneath any symbol layer.
         var layers = map.getStyle().layers;
 
@@ -67,28 +69,147 @@ function initMap() {
     });
 }
 
+function getMousePointer() {
+    map.on('click', function (e) {
+        let lng = JSON.stringify(e.lngLat.lng);
+        let lat = JSON.stringify(e.lngLat.lat);
+    })
+}
+
 function addMarkersToMap() {
     // add markers to map
     for (let feature of geojson.features) {
+        /*
+            // create a HTML element for each feature
+        let el = document.createElement('div');
+        el.className = 'marker';
 
+        // make a marker for each feature and add to the map
+        new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+        new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+            .setPopup(new mapboxgl.Popup({
+                    offset: 25
+                }) // add popups
+                .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>'))
+            .addTo(map);
+        */
+        
+        addMarker(feature.geometry.coordinates, feature.properties.title, feature.properties.description, feature.properties.className = "marker");
     }
 }
 
 function addMarker(coordinates, title, description, className) {
     let el = document.createElement('div');
     el.className = className;
-
+    
     new mapboxgl.Marker(el)
         .setLngLat(coordinates)
-        .setPopup(new mapboxgl.Popup({
-                offset: 25
-            }) // add popups
-            .setHTML('<h3>' + title + '</h3><p>' + description + '</p>'))
+        .setPopup(new mapboxgl.Popup({ offset : 25}) // add popups
+        .setHTML('<h3>' + title + '</h3><p>' + description + '</p>'))
         .addTo(map);
 }
 
 function loadMarkers() {
+    /*const coffeeShops = [
+        {
+            latitude: 43.084156,
+            longitude: -77.67514,
+            title: "Artesano Bakery & Cafe"
+	},
 
+        {
+            latitude: 43.083866,
+            longitude: -77.66901,
+            title: "Beanz"
+	},
+
+        {
+            latitude: 43.082520,
+            longitude: -77.67980,
+            title: "Starbucks"
+	},
+
+        {
+            latitude: 43.086678,
+            longitude: -77.669014,
+            title: "The College Grind"
+	},
+
+        {
+            latitude: 43.082634,
+            longitude: -77.68004,
+            title: "The Cafe & Market at Crossroads"
+	},
+
+        {
+            latitude: 43.08382,
+            longitude: -77.674805,
+            title: "RITZ Sports Zone"
+	},
+
+        {
+            latitude: 43.086502,
+            longitude: -77.66912,
+            title: "The Commons"
+	},
+
+        {
+            latitude: 43.08324,
+            longitude: -77.68105,
+            title: "The Market at Global Village"
+	},
+
+        {
+            latitude: 43.08384,
+            longitude: -77.67457,
+            title: "Brick City Cafe"
+	},
+
+        {
+            latitude: 43.084904,
+            longitude: -77.6676,
+            title: "Corner Store"
+	},
+
+        {
+            latitude: 43.08464,
+            longitude: -77.680145,
+            title: "CTRL ALT DELi"
+	},
+
+        {
+            latitude: 43.08359,
+            longitude: -77.66921,
+            title: "Gracie's"
+	}
+];*/
+    let POI = [];
+    // now convert this data to GeoJSON
+    for (let shop of POI) {
+        // an "empty" GEOJSON feature
+        const newFeature = {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: []
+            },
+            properties: {
+                title: "",
+                description: ""
+            }
+        };
+
+        // add some properties for the current coffee shop
+        newFeature.geometry.coordinates[0] = shop.longitude;
+        newFeature.geometry.coordinates[1] = shop.latitude;
+        newFeature.properties.title = shop.title;
+
+        // push it on to the 'geojson' array
+        geojson.features.push(newFeature);
+    }
+    console.log(geojson.features);
 }
 
 function flyTo(center = [0, 0]) {
@@ -107,24 +228,9 @@ function setPitchAndBearing(pitch = 0, bearing = 0) {
     map.setBearing(bearing);
 }
 
-function changeMapColors() {
-    /*
-        Based on crime data selection, we will take different data from our JSON file to change the colors of the states in the map.
-    */
-}
-
-function changeCrimeTimeline() {
-    /*
-        If possible, we will change the timeline in where to see how crime was like at that 
-        time period. 
-        With doing so, we can change the JSON file we load in
-        ... OR ...
-        We can have the data stored in the same JSON file and just change all the data to grab that information instead.
-    */
-}
-
 export {
     initMap,
+    getMousePointer,
     loadMarkers,
     addMarkersToMap,
     flyTo,
